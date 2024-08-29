faedf699afdf90530dc5cbc1ec40726ab1187b47
yumu31 <yumu31pj@gmail.com>
2024-08-29 23:41:07 +0900

    feat: Modify models.py to change Model name in admin

--- backend/tvs_backend/apps/news/admin.py
+++ backend/tvs_backend/apps/news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('news_id', 'news_title', 'news_body')
--- backend/tvs_backend/apps/news/models.py
+++ backend/tvs_backend/apps/news/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib import admin
 
 class News(models.Model):
     news_id = models.AutoField(primary_key=True)
     news_title = models.CharField(max_length=250)
     news_body = models.TextField()
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.news_title
