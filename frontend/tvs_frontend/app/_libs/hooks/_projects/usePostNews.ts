f8ccb7c63cefd9ae4faf2653bd43e71315b95dc7
yumu31 <yumu31pj@gmail.com>
2024-08-29 23:07:29 +0900

    feat: Add pgAdmin 4 and Test Post method

--- frontend/tvs_frontend/app/_libs/hooks/_projects/usePostNews.ts
+++ frontend/tvs_frontend/app/_libs/hooks/_projects/usePostNews.ts
@@ -1,29 +1,27 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-const usePostNews = (postData: any) => {
-  const [isPosting, setIsPosting] = useState(true);
+const usePostNews = () => {
+  const [isPosting, setIsPosting] = useState(false);
   const [postResponse, setPostResponse] = useState<any>();
   const [hasPostError, setHasPostError] = useState(false);
   const postErrorMessage = "Failed to post content";
 
-  useEffect(() => {
-    const postNews = async() => {
-      try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_BE_BASEURL}/api/news/`, postData);
-        setPostResponse(response);
-      } catch (e) {
-        console.error(e);
-        setHasPostError(true);
-      } finally {
-        setIsPosting(false)
-      }
-    };
+  const postNews = async (postData: any) => {
+    setIsPosting(true);
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_BE_BASEURL}/api/news/`, postData);
+      setPostResponse(response.data);
+      setHasPostError(false);
+    } catch (e) {
+      console.error(e);
+      setHasPostError(true);
+    } finally {
+      setIsPosting(false);
+    }
+  };
 
-    postNews();
-  }, [])
-
-  return {isPosting, postResponse, hasPostError, postErrorMessage};
-}
+  return { postNews, isPosting, postResponse, hasPostError, postErrorMessage };
+};
 
 export default usePostNews;
--- frontend/tvs_frontend/app/page.tsx
+++ frontend/tvs_frontend/app/page.tsx
@@ -1,37 +1,53 @@
 'use client';
 import useFetchNewsAll from "@/_libs/hooks/_projects/useFetchNewsAll.hooks";
+import { FormEventHandler } from "react";
 import usePostNews from "./_libs/hooks/_projects/usePostNews";
 
 const Home = () => {
-  const {isLoading, response, hasError, errorMessage} = useFetchNewsAll();
-
-  const postData = {
-    news_title: "Post test data title",
-    news_body: "post test data body<br>post test data body's line2"
-  }
-
-  const {isPosting, postResponse, hasPostError, postErrorMessage} = usePostNews(postData);
-
-  console.log(postErrorMessage);
+  const { isLoading, response, hasError, errorMessage } = useFetchNewsAll();
 
   return (
     <>
       {response ? (
-        <>  
+        <>
           {JSON.stringify(response)}
         </>
       ) : null}
-
-      <br />
-      <div>- - - - - - - - -</div>
-
-      {postResponse ? (
-        <>
-          {JSON.stringify(postResponse)}
-        </>
-      ) : null}
+      <NewsForm />
     </>
   );
+}
+
+const NewsForm = () => {
+  const { postNews, isPosting, hasPostError, postErrorMessage } = usePostNews();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    const title = form.get("title") || "";
+    const body = form.get("body") || "";
+    const postData = {
+      news_title: title,
+      news_body: body
+    };
+
+    try {
+      await postNews(postData);
+      window.location.reload();
+    } catch (error) {
+      console.error(postErrorMessage);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="title" style={{border: "1px solid #333", display: "block"}} defaultValue="" />
+      <textarea name="body" style={{border: "1px solid #333", display: "block"}} defaultValue="" />
+      <button type="submit" style={{border: "1px solid #333"}}>
+        submit
+      </button>
+    </form>
+  )
 }
 
 export default Home;
