8020b912df4e5d463cab9de091d38b57ec8a5aa2
yumu31 <yumu31pj@gmail.com>
2024-09-01 17:21:55 +0900

    Add serializer for CustomUser model

--- backend/tvs_backend/routers.py
+++ backend/tvs_backend/routers.py
@@ -1,5 +1,7 @@
 from rest_framework import routers
 from tvs_backend.apps.news.viewsets import NewsViewSet
+from tvs_backend.apps.users.viewsets import CustomUserViewSet
 
 router = routers.DefaultRouter()
-router.register('news', NewsViewSet) 
+router.register(r'news', NewsViewSet) 
+router.register(r'users', CustomUserViewSet)
