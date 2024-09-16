a45c286110a36dc7907566e73eb2056756e0b955
yumu31pj <yumu31pj@gmail.com>
2024-09-16 11:31:26 +0900

    Add post-news page component

--- frontend/tvs_frontend/app/page.tsx
+++ frontend/tvs_frontend/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { getFormatedDateString } from "_31pr-bootstrap/src/utils/getFormatedDateString";
 import useFetchNewsAll from "_project/hooks/useFetchNewsAll.hooks";
 
 const Home = () => {
@@ -10,9 +11,10 @@ const Home = () => {
         <>
           {response.map((item:any, key:number) => (
             <div key={key} style={{display: "flex", gap: "15px"}}>
+              <p>{item.news_id}</p>
               <h3>{item.news_title}</h3>
               <p>{item.news_body}</p>
-              <p>{item.updated_at}</p>
+              <p>{getFormatedDateString(item.updated_at)}</p>
             </div>
           ))}
         </>
